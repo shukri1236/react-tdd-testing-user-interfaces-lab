646cbd1b313b55bc784d184c86cd1f871b42994b
shukri1236 <shukri1236>
2024-08-18 22:20:26 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm (your name)</h1>
+      <img alt="My profile pic" src="https://via.placeholder.com/350" />
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud
+      </p>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  )
 }
 
 export default App;
